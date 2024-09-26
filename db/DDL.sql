56bb00b7a399694083ae9e829cb18a664d9cfd87
kbeomjun <98301971+kbeomjun@users.noreply.github.com>
2024-09-26 13:21:45 +0900

    DB - DDL 파일 추가(cascade 없음)

---
+++ db/DDL.sql
@@ -0,0 +1,283 @@
+drop database if exists fitness;
+create database fitness;
+use fitness;
+
+drop table if exists `member`;
+﻿CREATE TABLE `member` (
+	`me_id`			varchar(100) 	primary key,
+	`me_pw`			varchar(100)	not NULL,
+	`me_email`		varchar(255)	NOT NULL unique,
+	`me_name`		varchar(100)	not NULL,
+	`me_phone`		varchar(100)	not NULL,
+	`me_address`	varchar(255)	not NULL,
+	`me_birth`		date			not NULL,
+	`me_gender`		char(2)			not NULL,
+	`me_authority`	varchar(10)		NOT NULL,
+	`me_cookie`		varchar(255)	NULL,
+	`me_limit`		datetime		NULL,
+	`me_noshow`		int				not NULL default 0,
+	`me_cancel`		datetime		NULL
+);
+
+drop table if exists `branch`;
+CREATE TABLE `branch` (
+	`br_name`		varchar(100) 	primary key,
+	`br_phone`		varchar(255)	not NULL,
+	`br_address`	varchar(255)	not NULL,
+	`br_detail`		longtext		NULL
+);
+
+drop table if exists `employee`;
+CREATE TABLE `employee` (
+	`em_num`		int 			primary key auto_increment,
+	`em_name`		varchar(100)	not NULL,
+	`em_phone`		varchar(255)	not NULL,
+	`em_email`		varchar(255)	not NULL,
+	`em_gender`		char(2)			not NULL,
+	`em_position`	varchar(100)	not NULL,
+	`em_join`		datetime		not NULL default current_timestamp,
+	`em_address`	varchar(255)	not NULL,
+	`em_fi_name`	varchar(255)	NULL,
+	`em_br_name`	varchar(100)	NOT NULL
+);
+
+drop table if exists `branch_file`;
+CREATE TABLE `branch_file` (
+	`bf_num`		int 			primary key auto_increment,
+	`bf_ori_name`	varchar(255)	NULL,
+	`bf_name`		varchar(255)	NULL,
+	`bf_br_name`	varchar(100)	NOT NULL
+);
+
+drop table if exists `sports_equipment`;
+CREATE TABLE `sports_equipment` (
+	`se_name`		varchar(100) 	primary key,
+	`se_fi_name`	varchar(255)	not NULL
+);
+
+drop table if exists `branch_equipment_stock`;
+CREATE TABLE `branch_equipment_stock` (
+	`be_num`		int 			primary key auto_increment,
+	`be_amount`		int				not NULL,
+	`be_birth`		date			not NULL,
+	`be_record`		datetime		not NULL default current_timestamp,
+	`be_type`		char(2)			not NULL,
+	`be_br_name`	varchar(100)	NOT NULL,
+	`be_se_name`	varchar(100)	NOT NULL
+);
+
+drop table if exists `sports_program`;
+CREATE TABLE `sports_program` (
+	`sp_name`		varchar(100) 	primary key,
+	`sp_detail`		longtext		NULL
+);
+
+drop table if exists `branch_program`;
+CREATE TABLE `branch_program` (
+	`bp_num`		int 			primary key auto_increment,
+	`bp_total`		int				not NULL,
+	`bp_br_name`	varchar(100)	NOT NULL,
+	`bp_sp_name`	varchar(100)	NOT NULL,
+	`bp_em_num`		int				NOT NULL
+);
+
+drop table if exists `review_post`;
+CREATE TABLE `review_post` (
+	`rp_num`		int 			primary key auto_increment	NOT NULL,
+	`rp_title`		varchar(255)	not NULL,
+	`rp_content`	longtext		not NULL,
+    `rp_date`		datetime		not NULL default current_timestamp,
+	`rp_br_name`	varchar(100)	NOT NULL,
+	`rp_pa_num`		int				NOT NULL
+);
+
+drop table if exists `member_inquiry`;
+CREATE TABLE `member_inquiry` (
+	`mi_num`		int 			primary key auto_increment,
+	`mi_title`		varchar(255)	not NULL,
+	`mi_content`	longtext		not NULL,
+	`mi_state`		varchar(100)	not NULL default '답변대기중',
+	`mi_email`		varchar(100)	not NULL,
+    `mi_date`		datetime		not NULL default current_timestamp,
+	`mi_br_name`	varchar(100)	NOT NULL,
+	`mi_it_name`	varchar(100)	NOT NULL
+);
+
+drop table if exists `branch_order`;
+CREATE TABLE `branch_order` (
+	`bo_num`		int 			primary key auto_increment,
+	`bo_amount`		int				not NULL,
+	`bo_state`		varchar(100)	not NULL,
+	`bo_date`		datetime		not NULL default current_timestamp,
+	`bo_br_name`	varchar(100)	NOT NULL,
+	`bo_se_name`	varchar(100)	not NULL
+);
+
+drop table if exists `program_reservation`;
+CREATE TABLE `program_reservation` (
+	`pr_num`		int 			primary key auto_increment,
+	`pr_date`		datetime		not null default current_timestamp,
+	`pr_me_id`		varchar(100)	NOT NULL,
+	`pr_bs_num`		int				NOT NULL
+);
+
+drop table if exists `inquiry_type`;
+CREATE TABLE `inquiry_type` (
+	`it_name`		varchar(100) 	primary key
+);
+
+drop table if exists `payment`;
+CREATE TABLE `payment` (
+	`pa_num`		int 			primary key auto_increment,
+	`pa_date`		datetime		not NULL default current_timestamp,
+	`pa_price`		int				not NULL,
+	`pa_start`		datetime		not NULL,
+	`pa_end`		datetime		not NULL,
+	`pa_review`		char(1)			not NULL default 'N',
+	`pa_me_id`		varchar(100)	NOT NULL,
+	`pa_pt_num`		int				NOT NULL
+);
+
+drop table if exists `payment_type`;
+CREATE TABLE `payment_type` (
+	`pt_num`		int 			primary key auto_increment,
+	`pt_type`		varchar(100)	not NULL,
+	`pt_duration`	int				not NULL,
+	`pt_price`		int				not NULL
+);
+
+drop table if exists `branch_program_schedule`;
+CREATE TABLE `branch_program_schedule` (
+	`bs_num`		int 			primary key auto_increment,
+	`bs_start`		datetime		not NULL,
+	`bs_end`		datetime		not NULL,
+	`bs_current`	int				not NULL default 0,
+	`bs_bp_num`		int				NOT NULL
+);
+
+ALTER TABLE `employee` ADD CONSTRAINT `FK_branch_TO_employee_1` FOREIGN KEY (
+	`em_br_name`
+)
+REFERENCES `branch` (
+	`br_name`
+);
+
+ALTER TABLE `branch_file` ADD CONSTRAINT `FK_branch_TO_branch_file_1` FOREIGN KEY (
+	`bf_br_name`
+)
+REFERENCES `branch` (
+	`br_name`
+);
+
+ALTER TABLE `branch_equipment_stock` ADD CONSTRAINT `FK_branch_TO_branch_equipment_stock_1` FOREIGN KEY (
+	`be_br_name`
+)
+REFERENCES `branch` (
+	`br_name`
+);
+
+ALTER TABLE `branch_equipment_stock` ADD CONSTRAINT `FK_sports_equipment_TO_branch_equipment_stock_1` FOREIGN KEY (
+	`be_se_name`
+)
+REFERENCES `sports_equipment` (
+	`se_name`
+);
+
+ALTER TABLE `branch_program` ADD CONSTRAINT `FK_branch_TO_branch_program_1` FOREIGN KEY (
+	`bp_br_name`
+)
+REFERENCES `branch` (
+	`br_name`
+);
+
+ALTER TABLE `branch_program` ADD CONSTRAINT `FK_sports_program_TO_branch_program_1` FOREIGN KEY (
+	`bp_sp_name`
+)
+REFERENCES `sports_program` (
+	`sp_name`
+);
+
+ALTER TABLE `branch_program` ADD CONSTRAINT `FK_employee_TO_branch_program_1` FOREIGN KEY (
+	`bp_em_num`
+)
+REFERENCES `employee` (
+	`em_num`
+);
+
+ALTER TABLE `review_post` ADD CONSTRAINT `FK_branch_TO_review_post_1` FOREIGN KEY (
+	`rp_br_name`
+)
+REFERENCES `branch` (
+	`br_name`
+);
+
+ALTER TABLE `review_post` ADD CONSTRAINT `FK_payment_TO_review_post_1` FOREIGN KEY (
+	`rp_pa_num`
+)
+REFERENCES `payment` (
+	`pa_num`
+);
+
+ALTER TABLE `member_inquiry` ADD CONSTRAINT `FK_branch_TO_member_inquiry_1` FOREIGN KEY (
+	`mi_br_name`
+)
+REFERENCES `branch` (
+	`br_name`
+);
+
+ALTER TABLE `member_inquiry` ADD CONSTRAINT `FK_inquiry_type_TO_member_inquiry_1` FOREIGN KEY (
+	`mi_it_name`
+)
+REFERENCES `inquiry_type` (
+	`it_name`
+);
+
+ALTER TABLE `branch_order` ADD CONSTRAINT `FK_branch_TO_branch_order_1` FOREIGN KEY (
+	`bo_br_name`
+)
+REFERENCES `branch` (
+	`br_name`
+);
+
+ALTER TABLE `program_reservation` ADD CONSTRAINT `FK_member_TO_program_reservation_1` FOREIGN KEY (
+	`pr_me_id`
+)
+REFERENCES `member` (
+	`me_id`
+);
+
+ALTER TABLE `program_reservation` ADD CONSTRAINT `FK_branch_program_schedule_TO_program_reservation_1` FOREIGN KEY (
+	`pr_bs_num`
+)
+REFERENCES `branch_program_schedule` (
+	`bs_num`
+);
+
+ALTER TABLE `payment` ADD CONSTRAINT `FK_member_TO_payment_1` FOREIGN KEY (
+	`pa_me_id`
+)
+REFERENCES `member` (
+	`me_id`
+);
+
+ALTER TABLE `payment` ADD CONSTRAINT `FK_payment_type_TO_payment_1` FOREIGN KEY (
+	`pa_pt_num`
+)
+REFERENCES `payment_type` (
+	`pt_num`
+);
+
+ALTER TABLE `membership_payment` ADD CONSTRAINT `FK_membership_TO_membership_payment_1` FOREIGN KEY (
+	`ms_num`
+)
+REFERENCES `membership` (
+	`ms_num`
+);
+
+ALTER TABLE `branch_program_schedule` ADD CONSTRAINT `FK_branch_program_TO_branch_program_schedule_1` FOREIGN KEY (
+	`bs_bp_num`
+)
+REFERENCES `branch_program` (
+	`bp_num`
+);
+
