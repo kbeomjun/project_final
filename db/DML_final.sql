e606c7cd6f75992200e21a947ae25318b06c8a6a
HU2232 <sujifi@naver.com>
2024-09-27 16:56:53 +0900

    수정사항

--- db/DML_final.sql
+++ db/DML_final.sql
@@ -133,13 +133,8 @@ insert into `payment_type` (pt_type, pt_date, pt_count, pt_price)
 	values
 		('1개월 이용권', 30, 1, 300000),
 		('3개월 이용권', 90, 1, 900000),
-        ('6개월 이용권', 180, 1, 1200000);
-
-
+        ('6개월 이용권', 180, 1, 1200000),
 #PT 이용권 추가(결제 유형, 기간(30일), 횟수(30일 내 10회 수강), 가격)
-delete from `payment_type`;
-insert into `payment_type` (pt_type, pt_date, pt_count, pt_price)
-	values
 		('10회', 30, 10, 600000),
         ('20회', 60, 20, 1200000),
         ('30회', 90, 30, 1800000);
@@ -199,4 +194,5 @@ insert into `review_post` (rp_title, rp_content, rp_br_name, rp_pa_num)
 		('서비스가 좋았습니다', '최고예요', '역삼점', '1'),
         ('서비스가 그냥 그랬습니다', '다닐만은 해요', '강남점', '2'),
         ('서비스가 별로 안좋았습니다', '비추천합니다', '신논현점', '3');
+        
         
