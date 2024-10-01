f541de639dc440bdf91e347ffc72c054118d6387
suvin5027 <123538283+suvin5027@users.noreply.github.com>
2024-10-01 15:38:41 +0900

    [psv] 회원권 조회 구현 중
    [psv] 회원권 조회 구현 중

--- finalProject/src/main/java/kr/kh/fitness/controller/MembershipController.java
+++ finalProject/src/main/java/kr/kh/fitness/controller/MembershipController.java
@@ -1,18 +1,34 @@
 package kr.kh.fitness.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import kr.kh.fitness.model.vo.PaymentTypeVO;
+import kr.kh.fitness.service.MembershipService;
 
 // 회원권 컨트롤러
 @Controller
 @RequestMapping("/membership")
 public class MembershipController {
 	
-
+	@Autowired
+	MembershipService membershipService;
+	
 	@GetMapping("/membershipList")
-	public String membershipList() {
+	public String membershipList(Model model) {
+		
+		List<PaymentTypeVO> paymentList = membershipService.getMembershipList();
+		
 		System.out.println("회원권 조회 화면에 들어옴");
+		System.out.println(paymentList);
+		
+		model.addAttribute("paymentList", paymentList);
+		
 		return "/membership/membershipList";
 	}
 }
